36ebee3e488e24aa4e584ae642e5fdd7b4fadf89
jaime lopez <moreno.jlopez@gmail.com>
2021-11-23 23:08:16 -0300

    App web connected to service backend

--- publishing/src/app/app.module.ts
+++ publishing/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaperListComponent } from './components/paper-list/paper-list.component';
@@ -14,9 +19,15 @@ import { PaperListComponent } from './components/paper-list/paper-list.component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- publishing/src/app/components/paper-list/paper-list.component.ts
+++ publishing/src/app/components/paper-list/paper-list.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
+import {HttpClient} from '@angular/common/http';
+import {Component, ViewChild, AfterViewInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, SortDirection} from '@angular/material/sort';
+import {merge, Observable, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import { Paper } from 'src/app/models/paper.model';
+import { PaperService } from "../../services/paper.service";
+/**
+ * @title Table retrieving data through HTTP
+ */
+ @Component({
   selector: 'app-paper-list',
+  styleUrls: ['./paper-list.component.css'],
   templateUrl: './paper-list.component.html',
-  styleUrls: ['./paper-list.component.css']
 })
-export class PaperListComponent implements OnInit {
+export class PaperListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['title', 'author', 'created_at', 'remove' ];
 
-  constructor() { }
+  data: Paper[] = [];
 
-  ngOnInit(): void {
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  @ViewChild(MatPaginator)
+   paginator!: MatPaginator;
+  @ViewChild(MatSort)
+   sort!: MatSort;
+
+  constructor(private _paperService: PaperService) {}
+
+  ngAfterViewInit() {
+  
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this._paperService.getAll()
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this._paperService.getAll().pipe(catchError(() => observableOf(null)));
+        }),
+        map(data => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = data === null;
+
+          if (data === null) {
+            return [];
+          }
+
+          // Only refresh the result length if there is new data. In case of rate
+          // limit errors, we do not want to reset the paginator to zero, as that
+          // would prevent users from re-triggering requests.
+          return data;
+        }),
+      )
+      .subscribe(data => (this.data = data));
   }
-
 }
